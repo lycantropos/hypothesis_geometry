9f9e1e8a6eed9135b043053b775a539588ccc136
Azat Ibrakov <azatibrakov@gmail.com>
2020-02-03 13:19:22 +0300

    Complete tests

--- tests/strategies/__init__.py
+++ tests/strategies/__init__.py
@@ -2,4 +2,6 @@ from .base import (coordinates_strategies,
                    coordinates_strategies_limits_types_with_sizes_pairs,
                    coordinates_strategies_with_limits_and_types,
                    coordinates_strategies_with_sizes_pairs,
+                   coordinates_strategy_limits_type_pairs_with_sizes_pairs,
+                   coordinates_strategy_with_limit_and_type_pairs,
                    data)
--- tests/test_convex_contours.py
+++ tests/test_convex_contours.py
@@ -15,6 +15,7 @@ from tests.utils import (CoordinatesLimitsType,
                          SizesPair,
                          contour_has_valid_size,
                          point_has_coordinates_in_range,
+                         point_has_coordinates_types,
                          point_has_valid_size)
 
 
@@ -31,8 +32,49 @@ def test_basic(coordinates_with_sizes_pair: Tuple[Strategy[Coordinate],
 
 
 @given(strategies.data,
-       strategies.coordinates_strategies_limits_types_with_sizes_pairs)
+       strategies.coordinates_strategy_limits_type_pairs_with_sizes_pairs)
 def test_properties(
+        data: DataObject,
+        coordinates_limits_type_pair_with_sizes_pair
+        : Tuple[Tuple[CoordinatesLimitsType, CoordinatesLimitsType], SizesPair]
+) -> None:
+    ((x_coordinates_limits_type, y_coordinates_limits_type),
+     (min_size, max_size)) = coordinates_limits_type_pair_with_sizes_pair
+    ((x_coordinates, (min_x_value, max_x_value)),
+     x_type) = x_coordinates_limits_type
+    ((y_coordinates, (min_y_value, max_y_value)),
+     y_type) = y_coordinates_limits_type
+
+    strategy = convex_contours(x_coordinates, y_coordinates,
+                               min_size=min_size,
+                               max_size=max_size)
+
+    result = data.draw(strategy)
+
+    assert isinstance(result, list)
+    assert contour_has_valid_size(result,
+                                  min_size=min_size,
+                                  max_size=max_size)
+    assert all(isinstance(vertex, tuple) for vertex in result)
+    assert all(point_has_valid_size(vertex) for vertex in result)
+    assert all(point_has_coordinates_types(vertex,
+                                           x_type=x_type,
+                                           y_type=y_type)
+               for vertex in result)
+    assert all(point_has_coordinates_in_range(vertex,
+                                              min_x_value=min_x_value,
+                                              max_x_value=max_x_value,
+                                              min_y_value=min_y_value,
+                                              max_y_value=max_y_value)
+               for vertex in result)
+    assert is_contour_strict(result)
+    assert not is_contour_non_convex(result)
+    assert is_non_self_intersecting_contour(result)
+
+
+@given(strategies.data,
+       strategies.coordinates_strategies_limits_types_with_sizes_pairs)
+def test_same_coordinates(
         data: DataObject,
         coordinates_limits_type_with_sizes_pair: Tuple[CoordinatesLimitsType,
                                                        SizesPair]) -> None:
@@ -52,12 +94,15 @@ def test_properties(
                                   max_size=max_size)
     assert all(isinstance(vertex, tuple) for vertex in result)
     assert all(point_has_valid_size(vertex) for vertex in result)
-    assert all(all(isinstance(coordinate, type_)
-                   for coordinate in vertex)
+    assert all(point_has_coordinates_types(vertex,
+                                           x_type=type_,
+                                           y_type=type_)
                for vertex in result)
     assert all(point_has_coordinates_in_range(vertex,
-                                              min_value=min_value,
-                                              max_value=max_value)
+                                              min_x_value=min_value,
+                                              max_x_value=max_value,
+                                              min_y_value=min_value,
+                                              max_y_value=max_value)
                for vertex in result)
     assert is_contour_strict(result)
     assert not is_contour_non_convex(result)
