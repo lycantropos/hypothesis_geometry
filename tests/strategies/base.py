e6f33545c951044109eba2cd24096d557bb9aa32
Azat Ibrakov <azatibrakov@gmail.com>
2020-02-04 15:24:18 +0300

    Complete test for 'contours' function

--- tests/strategies/base.py
+++ tests/strategies/base.py
@@ -1,6 +1,7 @@
 from decimal import Decimal
 from fractions import Fraction
 from functools import partial
+from operator import ne
 from typing import (Any,
                     Tuple,
                     Type)
@@ -11,7 +12,6 @@ from hypothesis_geometry.hints import (Coordinate,
                                        Strategy)
 from hypothesis_geometry.planar import TRIANGLE_SIZE
 from tests.utils import (Limits,
-                         SizesPair,
                          identity,
                          to_pairs)
 
@@ -36,12 +36,8 @@ coordinates_types = strategies.sampled_from(
 coordinates_strategies = strategies.sampled_from(
         [factory() for factory in coordinates_strategies_factories.values()])
 
-
-def to_sizes_pairs() -> Strategy[SizesPair]:
-    sizes = strategies.integers(TRIANGLE_SIZE, 100)
-    return (strategies.tuples(sizes, strategies.none())
-            | (strategies.tuples(sizes, sizes)
-               .map(sort_pair)))
+sizes = strategies.integers(TRIANGLE_SIZE, 100)
+invalid_sizes = strategies.integers(max_value=TRIANGLE_SIZE - 1)
 
 
 def sort_pair(pair: Tuple[Any, Any]) -> Tuple[Any, Any]:
@@ -49,7 +45,15 @@ def sort_pair(pair: Tuple[Any, Any]) -> Tuple[Any, Any]:
     return (first, second) if first < second else (second, first)
 
 
-sizes_pairs = to_sizes_pairs()
+sizes_pairs = (strategies.tuples(sizes, strategies.none())
+               | strategies.tuples(sizes, sizes).map(sort_pair))
+invalid_sizes_pairs = ((strategies.tuples(sizes, sizes)
+                        .filter(lambda sizes_pair: ne(*sizes_pair))
+                        .map(sort_pair)
+                        .map(reversed)
+                        .map(tuple))
+                       | (strategies.tuples(invalid_sizes, invalid_sizes)
+                          .map(sort_pair)))
 coordinates_strategies_with_sizes_pairs = strategies.tuples(
         coordinates_strategies, sizes_pairs)
 
@@ -95,3 +99,5 @@ coordinates_strategies_limits_types_with_sizes_pairs = strategies.tuples(
         coordinates_strategies_with_limits_and_types, sizes_pairs)
 coordinates_strategy_limits_type_pairs_with_sizes_pairs = strategies.tuples(
         coordinates_strategy_with_limit_and_type_pairs, sizes_pairs)
+coordinates_strategies_with_invalid_sizes_pairs = strategies.tuples(
+        coordinates_strategies, invalid_sizes_pairs)
