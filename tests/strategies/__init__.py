1dcf2b2cfeaa78ed8a80f734516cd3292e033aae
Azat Ibrakov <azatibrakov@gmail.com>
2020-02-04 15:24:18 +0300

    Complete test for 'contours' function

--- tests/strategies/__init__.py
+++ tests/strategies/__init__.py
@@ -1,5 +1,6 @@
 from .base import (coordinates_strategies,
                    coordinates_strategies_limits_types_with_sizes_pairs,
+                   coordinates_strategies_with_invalid_sizes_pairs,
                    coordinates_strategies_with_limits_and_types,
                    coordinates_strategies_with_sizes_pairs,
                    coordinates_strategy_limits_type_pairs_with_sizes_pairs,
