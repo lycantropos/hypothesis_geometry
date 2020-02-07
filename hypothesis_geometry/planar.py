ffcdfefcf8175d7a99c47bf2a3cac5d873c69d7b
Azat Ibrakov <azatibrakov@gmail.com>
2020-02-07 15:11:43 +0300

    Complete docstring for 'concave_contours' function

--- hypothesis_geometry/planar.py
+++ hypothesis_geometry/planar.py
@@ -523,6 +523,71 @@ def concave_contours(x_coordinates: Strategy[Coordinate],
         ``None`` for reusing x-coordinates strategy.
     :param min_size: lower bound for contour size.
     :param max_size: upper bound for contour size, ``None`` for unbound.
+
+    >>> from hypothesis import strategies
+    >>> from hypothesis_geometry import planar
+
+    For same coordinates' domain:
+
+    >>> min_coordinate, max_coordinate = -1., 1.
+    >>> coordinates_type = float
+    >>> coordinates = strategies.floats(min_coordinate, max_coordinate,
+    ...                                 allow_infinity=False,
+    ...                                 allow_nan=False)
+    >>> min_size, max_size = 5, 10
+    >>> contours = planar.concave_contours(coordinates,
+    ...                                    min_size=min_size,
+    ...                                    max_size=max_size)
+    >>> contour = contours.example()
+    >>> isinstance(contour, list)
+    True
+    >>> min_size <= len(contour) <= max_size
+    True
+    >>> all(isinstance(vertex, tuple) for vertex in contour)
+    True
+    >>> all(len(vertex) == 2 for vertex in contour)
+    True
+    >>> all(all(isinstance(coordinate, coordinates_type)
+    ...         for coordinate in vertex)
+    ...     for vertex in contour)
+    True
+    >>> all(all(min_coordinate <= coordinate <= max_coordinate
+    ...         for coordinate in vertex)
+    ...     for vertex in contour)
+    True
+
+    For different coordinates' domains:
+
+    >>> min_x_coordinate, max_x_coordinate = -1., 1.
+    >>> min_y_coordinate, max_y_coordinate = 10., 100.
+    >>> coordinates_type = float
+    >>> x_coordinates = strategies.floats(min_x_coordinate, max_x_coordinate,
+    ...                                   allow_infinity=False,
+    ...                                   allow_nan=False)
+    >>> y_coordinates = strategies.floats(min_y_coordinate, max_y_coordinate,
+    ...                                   allow_infinity=False,
+    ...                                   allow_nan=False)
+    >>> min_size, max_size = 5, 10
+    >>> contours = planar.contours(x_coordinates, y_coordinates,
+    ...                            min_size=min_size,
+    ...                            max_size=max_size)
+    >>> contour = contours.example()
+    >>> isinstance(contour, list)
+    True
+    >>> min_size <= len(contour) <= max_size
+    True
+    >>> all(isinstance(vertex, tuple) for vertex in contour)
+    True
+    >>> all(len(vertex) == 2 for vertex in contour)
+    True
+    >>> all(all(isinstance(coordinate, coordinates_type)
+    ...         for coordinate in vertex)
+    ...     for vertex in contour)
+    True
+    >>> all(min_x_coordinate <= vertex_x <= max_x_coordinate
+    ...     and min_y_coordinate <= vertex_y <= max_y_coordinate
+    ...     for vertex_x, vertex_y in contour)
+    True
     """
     _validate_sizes(min_size, max_size, MIN_CONCAVE_CONTOUR_SIZE)
     return (strategies.lists(points(x_coordinates, y_coordinates),
