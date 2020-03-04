8e0b8be805836697f01ff96d59314e134fbdff2d
Azat Ibrakov <azatibrakov@gmail.com>
2020-03-04 14:40:26 +0300

    Complete '_merge' function

--- hypothesis_geometry/core/triangular.py
+++ hypothesis_geometry/core/triangular.py
@@ -98,8 +98,8 @@ def _initialize_triangulation(points: Sequence[Point]) -> Triangulation:
 
 def _merge(base_edge: QuadEdge) -> None:
     while True:
-        left_candidate = _to_left_candidate(base_edge)
-        right_candidate = _to_right_candidate(base_edge)
+        left_candidate, right_candidate = (_to_left_candidate(base_edge),
+                                           _to_right_candidate(base_edge))
         if left_candidate is right_candidate is None:
             break
         elif (left_candidate is None
