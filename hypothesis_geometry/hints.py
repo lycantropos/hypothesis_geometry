99afd0179fbcb5058d33451f14eabb65dd677e4e
Azat Ibrakov <azatibrakov@gmail.com>
2020-04-10 13:34:12 +0300

    Complete type hints

--- hypothesis_geometry/hints.py
+++ hypothesis_geometry/hints.py
@@ -6,9 +6,10 @@ from hypothesis.strategies import SearchStrategy
 
 Strategy = SearchStrategy
 Coordinate = Real
+BoundingBox = Tuple[Coordinate, Coordinate, Coordinate, Coordinate]
 Point = Tuple[Coordinate, Coordinate]
 Segment = Tuple[Point, Point]
-BoundingBox = Tuple[Coordinate, Coordinate, Coordinate, Coordinate]
 Contour = List[Point]
 Polyline = List[Point]
-Polygon = Tuple[Contour, List[Contour]]
+Multicontour = List[Contour]
+Polygon = Tuple[Contour, Multicontour]
