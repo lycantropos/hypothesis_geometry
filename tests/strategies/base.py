d8a8e06aa0e4dba305ad083405dd01ef40458faa
Azat Ibrakov <azatibrakov@gmail.com>
2020-04-10 14:00:26 +0300

    Complete test for 'polygons' function

--- tests/strategies/base.py
+++ tests/strategies/base.py
@@ -66,7 +66,7 @@ def to_invalid_sizes_pairs(min_valid_size: int
 
 concave_contours_sizes_pairs = to_sizes_pairs(MIN_CONCAVE_CONTOUR_SIZE)
 convex_contours_sizes_pairs = to_sizes_pairs(TRIANGULAR_CONTOUR_SIZE)
-holes_lists_sizes_pairs = to_sizes_pairs(EMPTY_MULTICONTOUR_SIZE, 5)
+multicontours_sizes_pairs = to_sizes_pairs(EMPTY_MULTICONTOUR_SIZE, 5)
 polylines_sizes_pairs = to_sizes_pairs(MIN_POLYLINE_SIZE)
 non_valid_concave_contours_sizes_pairs = to_non_valid_sizes_pairs(
         MIN_CONCAVE_CONTOUR_SIZE)
@@ -77,7 +77,8 @@ invalid_concave_contours_sizes_pairs = to_invalid_sizes_pairs(
         MIN_CONCAVE_CONTOUR_SIZE)
 invalid_convex_contours_sizes_pairs = to_invalid_sizes_pairs(
         TRIANGULAR_CONTOUR_SIZE)
-invalid_holes_list_sizes_pairs = to_invalid_sizes_pairs(EMPTY_MULTICONTOUR_SIZE)
+invalid_multicontours_sizes_pairs = to_invalid_sizes_pairs(
+        EMPTY_MULTICONTOUR_SIZE)
 invalid_polylines_sizes_pairs = to_invalid_sizes_pairs(MIN_POLYLINE_SIZE)
 
 
