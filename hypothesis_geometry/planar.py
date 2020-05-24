ce3528a88bc007287a3b3c6f6f7c26b8c8588e27
Azat Ibrakov <azatibrakov@gmail.com>
2020-05-24 20:37:00 +0300

    Complete 'star_contours' function

--- hypothesis_geometry/planar.py
+++ hypothesis_geometry/planar.py
@@ -1006,20 +1006,22 @@ def bounding_boxes(x_coordinates: Strategy[Coordinate],
 def star_contours(x_coordinates: Strategy[Coordinate],
                   y_coordinates: Optional[Strategy[Coordinate]] = None,
                   *,
-                  min_size: int = MIN_CONCAVE_CONTOUR_SIZE,
+                  min_size: int = TRIANGULAR_CONTOUR_SIZE,
                   max_size: Optional[int] = None,
                   ) -> Strategy[Contour]:
-    _validate_sizes(min_size, max_size, MIN_CONCAVE_CONTOUR_SIZE)
+    _validate_sizes(min_size, max_size, TRIANGULAR_CONTOUR_SIZE)
     min_size = max(min_size, MIN_CONCAVE_CONTOUR_SIZE)
-    return (strategies.lists(points(x_coordinates, y_coordinates),
+    return ((strategies.lists(points(x_coordinates, y_coordinates),
+                              min_size=min_size,
+                              max_size=max_size,
+                              unique=True)
+             .filter(points_do_not_lie_on_the_same_line)
+             .map(to_star_contour)
+             .filter(partial(_has_valid_size,
                              min_size=min_size,
-                             max_size=max_size,
-                             unique=True)
-            .filter(points_do_not_lie_on_the_same_line)
-            .map(to_star_contour)
-            .filter(partial(_has_valid_size,
-                            min_size=min_size,
-                            max_size=max_size)))
+                             max_size=max_size)))
+            if max_size is None or max_size > TRIANGULAR_CONTOUR_SIZE
+            else triangular_contours(x_coordinates, y_coordinates))
 
 
 EMPTY_MULTICONTOUR_SIZE = 0
