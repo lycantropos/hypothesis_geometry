809772ec6506d710ac0dfc117fa35d5f1ddd8923
Azat Ibrakov <azatibrakov@gmail.com>
2021-04-14 23:48:21 +0300

    Complete type hints

--- hypothesis_geometry/core/hints.py
+++ hypothesis_geometry/core/hints.py
@@ -5,6 +5,7 @@ from typing import (Callable,
 from ground.base import Orientation
 from ground.hints import Point
 
-Orienteer = Callable[[Point, Point, Point], Orientation]
 Domain = TypeVar('Domain')
+Range = TypeVar('Range')
 Chooser = Callable[[Sequence[Domain]], Domain]
+Orienteer = Callable[[Point, Point, Point], Orientation]
