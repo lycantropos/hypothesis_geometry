76863349d6b73820fc12eec1fb0995eb366a702f
Azat Ibrakov <azatibrakov@gmail.com>
2023-05-13 18:06:11 +0200

    Complete metadata

--- setup.py
+++ setup.py
@@ -1,39 +1,9 @@
-from pathlib import Path
-
 from setuptools import (find_packages,
                         setup)
-
-import hypothesis_geometry
 
 project_base_url = 'https://github.com/lycantropos/hypothesis_geometry/'
 
 
-def read_file(path_string: str) -> str:
-    return Path(path_string).read_text(encoding='utf-8')
-
-
-setup(name=hypothesis_geometry.__name__,
-      packages=find_packages(exclude=('tests', 'tests.*')),
-      version=hypothesis_geometry.__version__,
-      description=hypothesis_geometry.__doc__,
-      long_description=read_file('README.md'),
-      long_description_content_type='text/markdown',
-      author='Azat Ibrakov',
-      author_email='azatibrakov@gmail.com',
-      classifiers=[
-          'Framework :: Hypothesis',
-          'Intended Audience :: Developers',
-          'License :: OSI Approved :: MIT License',
-          'Programming Language :: Python :: 3.6',
-          'Programming Language :: Python :: 3.7',
-          'Programming Language :: Python :: 3.8',
-          'Programming Language :: Python :: 3.9',
-          'Programming Language :: Python :: 3.10',
-          'Programming Language :: Python :: Implementation :: CPython',
-          'Programming Language :: Python :: Implementation :: PyPy',
-      ],
-      license='MIT License',
+setup(packages=find_packages(exclude=('tests', 'tests.*')),
       url=project_base_url,
-      download_url=project_base_url + 'archive/master.zip',
-      python_requires='>=3.6',
-      install_requires=read_file('requirements.txt'))
+      download_url=project_base_url + 'archive/master.zip')
